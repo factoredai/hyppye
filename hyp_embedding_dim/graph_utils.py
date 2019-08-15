448159410e0ae6ced00a293b80421aa55b87768e
secg95 <secg95@gmail.com>
2019-08-15 14:30:44 -0500

    precision arithmetic added to phyton.py and graphutil.py

--- hyp_embedding_dim/graph_utils.py
+++ hyp_embedding_dim/graph_utils.py
@@ -1,8 +1,13 @@
 import numpy as np
+from mpmath import mp
 
 # Express a hyperbolic distance in the unit disk
 def hyp_to_euc_dist(x):
-    return np.sqrt((np.cosh(x) - np.float128(1))/(np.cosh(x) + np.float128(1)))
+    mp_cosh = np.vectorize(mp.cosh)
+    mp_sqrt = np.vectorize(mp.sqrt)
+    return mp_sqrt((mp_cosh(x) - 1))/(mp_cosh(x) + 1)
+
+
 
 def digits(n, pad):
     """
@@ -26,13 +31,15 @@ def max_degree(G):
     return [max_node, max_d]
 
 def place_children_codes(dim, n_children, Gen_matrices):
+
+
     r = int(np.ceil(np.log2(n_children)))
     n = 2**r - 1
 
     G = Gen_matrices[r-1]
 
     #generate the codewords using the matrix
-    C = np.zeros((n_children, dim), dtype=np.float128)
+    C = np.zeros((n_children, dim))
     for i in range(n_children):
         #codeword generated from matrix G
         cw = np.mod(np.dot(np.expand_dims(digits(i, pad=r), -1).T, G), 2)
@@ -40,12 +47,12 @@ def place_children_codes(dim, n_children, Gen_matrices):
         rep = int(np.floor(dim/n))
         for j in range(rep):
             #repeat it as many times as we can
-            C[[i], j*n:(j+1)*n] = cw.astype(np.float128)
+            C[[i], j*n:(j+1)*n] = cw
 
         rm = dim - rep*n
         if rm > 0:
-            C[[i], rep*n:dim] = cw[0, :rm].T.astype(np.float128)
+            C[[i], rep*n:dim] = cw[0, :rm].T
 
     # inscribe the unit hypercube vertices into unit hypersphere
-    points = (np.float128(1)/np.sqrt(dim)*(-1)**C).T
+    points = (1/np.sqrt(dim)*(-1)**C).T
     return points
