507cb8ff293cb0db85e301e9bcf3252ec1af19c0
Santiago Enrique Cortes Gomez <secg95@pop-os.localdomain>
2019-08-21 20:52:45 -0500

    comments in evaluation.py

--- hyppye/evaluation.py
+++ hyppye/evaluation.py
@@ -3,16 +3,35 @@ import networkx as nx
 import numpy as np
 import mpmath as mp
 
-###################STILL IN PROGRESSS#################################################################
+#verifies that the distortion can be computed
 def entry_is_good(h, h_rec): return (not np.isnan(h_rec)) and (not np.isinf(h_rec)) and h_rec != 0 and h != 0
 
 def distortion_entry(h,h_rec,me,mc):
+      """
+          computes the distortion for a single row
+          return:
+          @mc = maximum  distortion
+          @me = minimum distortion
+      """
     avg = abs(h_rec - h)/h
     if h_rec/h > me: me = h_rec/h
     if h/h_rec > mc: mc = h/h_rec
     return (avg,me,mc)
 
 def distortion_row(H1, H2, n, row):
+    """
+          computes the distortion metric(Wc and average) for  a row hence for a node
+          @ H1 = distances from the row to the rest of the graph(in the graph)
+          @ H2 = distances from the row to the rest of the graph(in the embbeding)
+          @ n = graph degree
+          @ row = row for which the map is being computed
+          return:
+          @mc = maximum  distortion
+          @me = minimum distortion
+          @_avg = average distortion
+          @good number of rows that the metric was computed
+      """
+
     mc, me, avg, good = 0,0,0,0
     for i in range(n):
         if i != row and entry_is_good(H1[i], H2[i]):
